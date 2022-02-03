826aab1f20c1574e340775d5cccc24b7975ff58c
Acxel Derian <acxelderian@gmail.com>
2022-02-03 15:51:52 +0700

    update php code in login.php

--- pages/login.php
+++ pages/login.php
@@ -1,11 +1,35 @@
 <?php
-    require("../util/connection.php");
-    $peserta = $conn->query("SELECT * From peserta")->fetch_all(MYSQLI_ASSOC);
-    
+require("../util/connection.php");
+$peserta = $conn->query("SELECT * From peserta")->fetch_all(MYSQLI_ASSOC);
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    if ($email == "admin" && $password == "adminTCC2022") {
+        //pindah ke admin
+        $_SESSION['email'] = $email;
+        header('Location: adminStart.php');
+    } else {
+        $password = md5($password);
+        $found = false;
+        foreach ($peserta as $key => $value) {
+            if ($email == $value['email'] && $password == $value['password']) {
+                $found = true;
+            }
+        }
+        if ($found) {
+            //pindah ke user
+            $_SESSION['email'] = $email;
+            header('Location:userStart.php');
+        } else {
+            echo "<script>alert('Email or Password is incorrect')</script>";
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,11 +38,12 @@
     <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="../tailwind/tailwind.css">
 </head>
+
 <body>
     <div class="w-full h-screen bg-tcc-darkBlue">
-        <?php require("../components/navbar.php")?>
+        <?php require("../components/navbar.php") ?>
         <div class="w-screen h-3/4 flex flex-col justify-center items-center">
-            <form action="" class="flex flex-col items-center justify-between gap-5 bg-tcc-darkGray py-16 px-10 rounded-lg w-1/3 h-3/4">
+            <form action="" class="flex flex-col items-center justify-between gap-5 bg-tcc-darkGray py-16 px-10 rounded-lg w-1/3 h-3/4" method="POST">
                 <h2 class="text-5xl font-bold">Login</h2>
                 <div class="flex flex-col items-center justify-center w-full gap-y-5 h-4/5">
                     <input type="text" name="email" placeholder="Email" class="px-4 py-2 w-4/5 rounded-md">
@@ -29,4 +54,5 @@
         </div>
     </div>
 </body>
+
 </html>
