ec4a37ba175dc49adfb9aa18fec78461652af0ea
mikhaelc09 <mikhael.chris09@gmail.com>
2022-02-03 13:49:54 +0700

    Make Admin Page
    make admin page, start navbar page

---
+++ components/adminnav.php
@@ -0,0 +1,9 @@
+<nav class="h-10 w-full pr-10 py-8 text-gray-100 flex justify-end items-center text-xl font-medium">
+    <ul class="flex flex-row gap-5 items-center">
+        <li class="transition-all hover:scale-110"><a href="">Home</a></li>
+        <li class="transition-all hover:scale-110"><a href="">Announcement</a></li>
+        <li class="transition-all hover:scale-110"><a href="">Quiz</a></li>
+        <li class="transition-all hover:scale-110"><a href="">Users</a></li>
+        <li class="transition-all hover:scale-110"><a href="">Logout</a></li>
+    </ul>
+</nav>
