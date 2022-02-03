e3a64d6d0753f152d2217286ad36428a5defb5da
Acxel Derian <acxelderian@gmail.com>
2022-02-03 16:04:01 +0700

    delete 1/3 in section class in adminStart.php

--- pages/adminStart.php
+++ pages/adminStart.php
@@ -17,7 +17,7 @@
 
         <main class="w-full flex flex-col items-center py-10 gap-4">
 
-            <a href="" class="w-1/3"><section class="w-1/3 h-48 rounded-lg shadow-md bg-tcc-darkGray cursor-pointer flex items-center justify-center hover:bg-tcc-lightGray hover:underline underline-offset-4">
+            <a href="" class="w-1/3"><section class="h-48 rounded-lg shadow-md bg-tcc-darkGray cursor-pointer flex items-center justify-center hover:bg-tcc-lightGray hover:underline underline-offset-4">
                 <h1 class="text-3xl font-bold">Announcement</h1>
             </section></a>
 
@@ -25,7 +25,7 @@
                 <h1 class="text-3xl font-bold">Quiz</h1>
             </section></a>
 
-            <a href="" class="w-1/3"><section class="w-1/3 h-48 rounded-lg shadow-md bg-tcc-darkGray cursor-pointer flex items-center justify-center hover:bg-tcc-lightGray hover:underline underline-offset-4">
+            <a href="" class="w-1/3"><section class="h-48 rounded-lg shadow-md bg-tcc-darkGray cursor-pointer flex items-center justify-center hover:bg-tcc-lightGray hover:underline underline-offset-4">
                 <h1 class="text-3xl font-bold">Users</h1>
             </section></a>
             
