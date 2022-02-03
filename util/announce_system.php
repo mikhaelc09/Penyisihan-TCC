3d99f51889b466df72b5bd7c95554f5a2b0d9240
DraSelXL <uni.nathanwill02@gmail.com>
2022-02-03 15:44:54 +0700

    add an announce class to modify MySQL database

---
+++ util/announce_system.php
@@ -0,0 +1,39 @@
+<?php
+   require_once './connection.php';
+   require_once './announcement.php';
+
+   class AnnounceSystem {
+      public static function addAnnouncement($announcement) {
+         if ($announcement instanceof Announcement) {
+            $title = $announcement->get_title();
+            $body = $announcement->get_body();
+            $dateNow = date('Y-m-d');
+            $status = $announcement -> get_status();
+
+            global $conn;
+            $stmt = $conn -> prepare("INSERT INTO announcement(judul, body, date_created, status) VALUES (?, ?, ?, ?)");
+            $stmt -> bind_param('ssss', $title, $body, $dateNow, $status);
+            $stmt -> execute();
+         }
+         else echo "Gagal add announcement";
+      }
+      public static function deleteAnnouncement($announcement) {
+         if ($announcement instanceof Announcement) {
+            echo "Berhasil jalan";
+         }
+         else echo "Gagal delete announcement";
+      }
+      public static function editAnnouncement($announcement) {
+         if ($announcement instanceof Announcement) {
+            echo "Berhasil jalan";
+         }
+         else echo "Gagal jalan";
+      }
+   }
+
+   if (isset($_POST['announce'])) {
+      print_r($_POST);
+      $newAnnounce = new Announcement($_POST['title'], $_POST['body']);
+      AnnounceSystem::addAnnouncement($newAnnounce) ;
+   }
+?>
