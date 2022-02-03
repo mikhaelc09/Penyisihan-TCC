a558bc7f42fa0021bc131ff139eab58c8a1789be
mikhaelc09 <mikhael.chris09@gmail.com>
2022-02-03 15:08:36 +0700

    add adminStartPage

--- pages/adminStart.php
+++ pages/adminStart.php
@@ -16,15 +16,19 @@
         <?php require("../components/adminnav.php")?>
 
         <main class="w-full flex flex-col items-center py-10 gap-4">
-            <section class="w-1/3 h-48 rounded-lg shadow-md bg-tcc-darkGray cursor-pointer flex items-center justify-center hover:bg-tcc-lightGray hover:underline underline-offset-4">
+
+            <a href="" class="w-1/3"><section class="w-1/3 h-48 rounded-lg shadow-md bg-tcc-darkGray cursor-pointer flex items-center justify-center hover:bg-tcc-lightGray hover:underline underline-offset-4">
                 <h1 class="text-3xl font-bold">Announcement</h1>
-            </section>
-            <section class="w-1/3 h-48 rounded-lg shadow-md bg-tcc-darkGray cursor-pointer flex items-center justify-center hover:bg-tcc-lightGray hover:underline underline-offset-4">
+            </section></a>
+
+            <a href="../pages/quizSetup.php" class="w-1/3"><section class="h-48 rounded-lg shadow-md bg-tcc-darkGray cursor-pointer flex items-center justify-center hover:bg-tcc-lightGray hover:underline underline-offset-4">
                 <h1 class="text-3xl font-bold">Quiz</h1>
-            </section>
-            <section class="w-1/3 h-48 rounded-lg shadow-md bg-tcc-darkGray cursor-pointer flex items-center justify-center hover:bg-tcc-lightGray hover:underline underline-offset-4">
+            </section></a>
+
+            <a href="" class="w-1/3"><section class="w-1/3 h-48 rounded-lg shadow-md bg-tcc-darkGray cursor-pointer flex items-center justify-center hover:bg-tcc-lightGray hover:underline underline-offset-4">
                 <h1 class="text-3xl font-bold">Users</h1>
-            </section>
+            </section></a>
+            
         </main>
     </div>
 </body>
