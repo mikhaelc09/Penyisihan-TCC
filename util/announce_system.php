da0169886a245f02f39f09e0a34ea2dc22a9f852
DraSelXL <uni.nathanwill02@gmail.com>
2022-02-04 10:25:29 +0700

    Move AnnounceSystem class to the Announcement.php file

--- util/announce_system.php
+++ util/announce_system.php
@@ -1,50 +1,5 @@
 <?php
-   require_once './connection.php';
    require_once './announcement.php';
-
-   /** This class is used to modify the database according to what the user needs. Currently it supports static methods for adding, editing, and deleting (although deleting from a database is discouraged!). */
-   class AnnounceSystem {
-      public static function addAnnouncement($announcement) {
-         if ($announcement instanceof Announcement) {
-            $title = $announcement->get_title();
-            $body = $announcement->get_body();
-            $dateNow = date('Y-m-d');
-            $status = $announcement -> get_status();
-
-            global $conn;
-            $stmt = $conn -> prepare("INSERT INTO announcement(judul, body, date_created, status) VALUES (?, ?, ?, ?)");
-            $stmt -> bind_param('sssi', $title, $body, $dateNow, $status);
-            $stmt -> execute();
-
-            echo 'Successfully add the announcement';
-         }
-         else echo "Gagal add announcement";
-      }
-      /** Avoid using this method at all cost, this is just here for emergency purposes only */
-      public static function deleteAnnouncement($id) {
-         global $conn;
-         $stmt = $conn -> prepare("DELETE FROM announcement WHERE (announcement_id = ?)");
-         $stmt -> bind_param('i', $id);
-         $stmt -> execute();
-
-         echo 'Successfully delete the announcement';
-      }
-      public static function editAnnouncement($id, $announcement) {
-         if ($announcement instanceof Announcement) {
-            $title = $announcement->get_title();
-            $body = $announcement->get_body();
-            $status = $announcement->get_status();
-
-            global $conn;
-            $stmt = $conn -> prepare("UPDATE announcement SET judul = ?, body = ?, status = ? where announcement_id = ?");
-            $stmt -> bind_param('ssii', $title, $body, $status, $id);
-            $stmt -> execute();
-
-            echo 'Successfully edit the announcement';
-         }
-         else echo "Gagal Edit";
-      }
-   }
 
    if (isset($_POST['announce'])) {
       $newAnnounce = new Announcement($_POST['title'], $_POST['body']);
--- util/announcement.php
+++ util/announcement.php
@@ -2,7 +2,7 @@
    require_once './connection.php';
    /**
     * A constant variable for the status property of the Announcement class.
-    * You can use it with calling the {AnnouncementObject}->get_status(ANNOUNCEMENT_STATUS['ACTIVE']); method.
+    * You can use it with calling the `{AnnouncementObject}->set_status(ANNOUNCEMENT_STATUS['ACTIVE']);` method.
     */
    define("ANNOUNCEMENT_STATUS", [
       'NOT_ACTIVE' => 0,
@@ -48,6 +48,50 @@
 
       function get_status() {
          return $this -> status;
+      }
+   }
+   
+   /** This class is used to modify the database according to what the user needs. Currently it supports static methods for adding, editing, and deleting (although deleting from a database is discouraged!). */
+   class AnnounceSystem {
+      public static function addAnnouncement($announcement) {
+         if ($announcement instanceof Announcement) {
+            $title = $announcement->get_title();
+            $body = $announcement->get_body();
+            $dateNow = date('Y-m-d');
+            $status = $announcement -> get_status();
+
+            global $conn;
+            $stmt = $conn -> prepare("INSERT INTO announcement(judul, body, date_created, status) VALUES (?, ?, ?, ?)");
+            $stmt -> bind_param('sssi', $title, $body, $dateNow, $status);
+            $stmt -> execute();
+
+            echo 'Successfully add the announcement';
+         }
+         else echo "Gagal add announcement";
+      }
+      /** Avoid using this method at all cost, this is just here for emergency purposes only */
+      public static function deleteAnnouncement($id) {
+         global $conn;
+         $stmt = $conn -> prepare("DELETE FROM announcement WHERE (announcement_id = ?)");
+         $stmt -> bind_param('i', $id);
+         $stmt -> execute();
+
+         echo 'Successfully delete the announcement';
+      }
+      public static function editAnnouncement($id, $announcement) {
+         if ($announcement instanceof Announcement) {
+            $title = $announcement->get_title();
+            $body = $announcement->get_body();
+            $status = $announcement->get_status();
+
+            global $conn;
+            $stmt = $conn -> prepare("UPDATE announcement SET judul = ?, body = ?, status = ? where announcement_id = ?");
+            $stmt -> bind_param('ssii', $title, $body, $status, $id);
+            $stmt -> execute();
+
+            echo 'Successfully edit the announcement';
+         }
+         else echo "Gagal Edit";
       }
    }
 ?>
