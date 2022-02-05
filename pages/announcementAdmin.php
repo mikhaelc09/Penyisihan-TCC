185fd0b910b4a5898990cce53f85eb52b7d69d10
DraSelXL <uni.nathanwill02@gmail.com>
2022-02-05 12:45:16 +0700

    Refactor adminNav and include it in announcement admin page

--- pages/announcementAdmin.php
+++ pages/announcementAdmin.php
@@ -18,9 +18,7 @@
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 <body class="bg-tcc-darkBlue">
-   <nav class="back-nav">
-      <a href="./adminStart.php">Go back</a>
-   </nav>
+   <?php require("../components/adminnav.php") ?>
    <aside class="announce-create">
       <h1 class="text-5xl">Create Announcement:</h1>
       <div class="announce-detail">
