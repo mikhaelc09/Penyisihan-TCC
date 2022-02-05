da39d45ba122545f4b8af6c6d2df229197003db4
Acxel Derian <acxelderian@gmail.com>
2022-02-05 16:29:51 +0700

    change status to line id in adminListUser.php

--- pages/userProfile.php
+++ pages/userProfile.php
@@ -24,19 +24,20 @@ if (isset($_SESSION['email']) && $_SESSION['email'] != "admin") {
 <body>
     <div class="w-full h-screen bg-tcc-darkBlue">
         <?php require("../components/adminnav.php") ?>
-        <div class="bg-gray-200 h-3/4 font-sans w-full flex flex-row justify-center items-center">
+        <div class="bg-tcc-darkBlue h-3/4 font-sans w-full flex flex-row justify-center items-center">
             <div class="card w-96 mx-auto bg-white  shadow-xl hover:shadow">
                 <img style="width:100px;height:100px" class="mx-auto rounded-full -mt-20" src="https://static.vecteezy.com/system/resources/previews/002/318/271/non_2x/user-profile-icon-free-vector.jpg" alt="">
                 <div class="text-center mt-2 text-3xl font-medium"><?= $user['full_name'] ?></div>
                 <div class="text-center mt-2 font-light text-sm"><?= $user['email'] ?></div>
                 <div class="text-center font-normal text-lg"><?= $user['nrp'] ?></div>
+                <div class="text-center font-light text-sm"><?= $user['line_id'] ?></div>
                 <div class="px-6 text-center mt-2 font-light text-sm">
                     <p class="my-2">
                         The Participant of Top Coder Competition (TCC) 2022
                     </p>
                 </div>
                 <hr class="mt-8">
-                <a style="background-color:blue" href="" class="flex p-4">
+                <a href="" class="flex p-4 bg-blue-600">
                     <div class="w-full text-center text-white">
                         Change Password
                     </div>
