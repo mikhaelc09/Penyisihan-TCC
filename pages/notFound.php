5278b2b64b67b79012798da84f121488d7465c40
mikhaelc09 <mikhael.chris09@gmail.com>
2022-02-06 20:34:11 +0700

    add pageNotFound

---
+++ pages/notFound.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page not Found</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../tailwind/tailwind.css">
+</head>
+<body>
+    <div class="w-full h-screen bg-tcc-darkBlue flex flex-col items-center justify-center gap-y-3">
+        <h1 class="text-5xl font-bold text-orange-200">Are you lost?</h1>
+        <h2 class="text-3xl text-orange-200">Do you know you can go back by clicking this button?</h2>
+        <a href="../pages/start.php"><button class="bg-tcc-orange px-4 py-2 font-bold rounded-lg">Go to Home</button></a>
+    </div>
+</body>
+</html>
